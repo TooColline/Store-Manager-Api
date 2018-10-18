e9300c29aacced0a081e35b810ab5c94e3a87581
Too Collins <collinstoo.ct@gmail.com>
2018-10-18 20:09:57 +0300

    [Feature #161203714] Add route for get all products

--- app/api/v1/views.py
+++ app/api/v1/views.py
@@ -1,8 +1,9 @@
 from flask_restful import Api, Resource
 
 from . import v1_blueprint
-from .resources import admin_endpoints
+from .resources import admin_endpoints, admin_and_store_attendant_endpoints
 
 API = Api(v1_blueprint)
 
 API.add_resource(admin_endpoints.Admin, '/products')
+API.add_resource(admin_and_store_attendant_endpoints.AdminAndStoreAttendant, '/products')
