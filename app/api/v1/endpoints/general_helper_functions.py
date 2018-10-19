6f55e97dd7f3325e961d995fbda2e3965da80f7d
Too Collins <collinstoo.ct@gmail.com>
2018-10-19 17:50:12 +0300

    [Feature #161203780] Set up helper functions for adding sale record

--- app/api/v1/endpoints/general_helper_functions.py
+++ app/api/v1/endpoints/general_helper_functions.py
@@ -1,7 +1,7 @@
 from flask import abort, jsonify, make_response
 from datetime import datetime
 
-from ..models import products
+from ..models import products, sales
 
 def json_null_request(data):
     """Abort, if data has no json object."""
@@ -62,3 +62,29 @@ def abort_if_not_found(product_id):
 
     abort(make_response(jsonify(
         message="Product with id number {} not found".format(product_id)), 404))
+
+def add_sale_record(name, price, quantity, totalamt):
+
+    """Creates a new sale record"""
+
+    if name and price and quantity and totalamt:
+        # If all the required data are provided
+        sale_id = len(sales.salerec) + 1
+        SaleOrder = {
+            'sale_id': sale_id,
+            'name': name,
+            'price': price,
+            'quantity': quantity,
+            'totalamt': totalamt,
+            'date_added': datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        }
+
+        sales.salerec.append(SaleOrder)
+        response = jsonify({
+            "message": "Sale record has been created successfully",
+            "SaleOrder": sales.salerec[-1]})
+        response.status_code = 201
+    else:
+        # if any of the required parameters is missing or none
+        miss_parameter_required()
+    return response
