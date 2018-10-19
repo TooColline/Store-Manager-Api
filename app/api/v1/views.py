411a17500cde5ad917d59a78a20041117a538940
Too Collins <collinstoo.ct@gmail.com>
2018-10-19 17:53:05 +0300

    [Feature #161203780] Add routes for saleorder

--- app/api/v1/views.py
+++ app/api/v1/views.py
@@ -1,10 +1,11 @@
 from flask_restful import Api, Resource
 
 from . import v1_blueprint
-from .endpoints import admin_endpoints, admin_and_store_attendant_endpoints
+from .endpoints import admin_endpoints, admin_and_store_attendant_endpoints, store_attendant_endpoint
 
 API = Api(v1_blueprint)
 
 API.add_resource(admin_endpoints.Admin, '/products')
 API.add_resource(admin_and_store_attendant_endpoints.AdminAndStoreAttendant, '/products')
 API.add_resource(admin_and_store_attendant_endpoints.GetSpecificProduct, '/products/<int:product_id>')
+API.add_resource(store_attendant_endpoint.StoreAttendant, '/saleorder')
