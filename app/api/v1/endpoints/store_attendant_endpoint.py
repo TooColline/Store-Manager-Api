75fc5a7fc1544d73746052d7fc8a0d7ce584cd99
Too Collins <collinstoo.ct@gmail.com>
2018-10-19 17:55:02 +0300

    [Feature #161203780] Refactor endpoints to pass test

---
+++ app/api/v1/endpoints/store_attendant_endpoint.py
@@ -0,0 +1,47 @@
+from flask import Flask, jsonify, request, abort, make_response
+from flask_restful import Resource
+
+from . import general_helper_functions
+from ..models import products
+
+class StoreAttendant(Resource):
+    """Simple class that holds the store endpoints"""
+
+    def post(self):
+        """POST /products"""
+                  
+        data = request.get_json()
+        general_helper_functions.json_null_request(data)
+        
+        try:
+            name = data['name']
+            price = data['price']
+            quantity = data['quantity']
+            totalamt = (price * quantity)
+
+        except KeyError:
+            general_helper_functions.miss_parameter_required()
+
+        if not isinstance(name, str):
+            abort(make_response(jsonify(
+                message="Bad request. The product name should be a string."
+            ), 400))
+        
+        if not isinstance(price, int):
+            abort(make_response(jsonify(
+                message="Bad request. The product price should be an integer."
+            ), 400))
+        
+        if price < 1:
+            abort(make_response(jsonify(
+                message="Bad request. The product price should be a positive integer."
+            ), 400))
+
+        if not isinstance(quantity, int):
+            abort(make_response(jsonify(
+                message="Bad request. The quantity should be an integer."
+            ), 400))
+
+        response = general_helper_functions.add_sale_record(name, price, quantity, totalamt)
+
+        return response
