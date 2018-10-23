4bc0987255384953a567afc3688c344109bc69ce
Too Collins <collinstoo.ct@gmail.com>
2018-10-23 12:12:38 +0300

    [Chore #161224557] Add users model seperately

---
+++ app/api/v1/models/UserModel.py
@@ -0,0 +1,27 @@
+'''This module holds all data and logic of the users in the application'''
+
+from datetime import datetime
+
+users = []
+
+
+class UserModel():
+    '''Initializes a new user'''
+    def __init__(self, email, password, role):
+        self.id = len(users) + 1
+        self.email = email
+        self.password = password
+        self.role = role
+
+    def getEmail(self):
+        return self.email
+        
+    def save(self):
+        '''Saves a user by appending them to the users list'''
+        new_user = {
+                "id": self.id,
+                "email": self.email,
+                "password": self.password,
+                "role": self.role
+            }
+        users.append(new_user)
