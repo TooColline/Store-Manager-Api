bc51d74886ada07593fc23569fa01d5538d58b1f
Too Collins <collinstoo.ct@gmail.com>
2018-10-24 12:07:28 +0300

    [Feature #161413761] Add auth routes

--- app/api/v1/views.py
+++ app/api/v1/views.py
@@ -1,9 +1,10 @@
 from flask_restful import Api, Resource
 
-from . import v1_blueprint
-from .endpoints import admin_endpoints, admin_and_store_attendant_endpoints, store_attendant_endpoint
+from . import v1_blueprint, auth_v1_blueprint
+from .endpoints import admin_endpoints, admin_and_store_attendant_endpoints, store_attendant_endpoint, authorization
 
 API = Api(v1_blueprint)
+API_AUTH = Api(auth_v1_blueprint)
 
 API.add_resource(admin_endpoints.Admin, '/products')
 API.add_resource(admin_and_store_attendant_endpoints.AdminAndStoreAttendant, '/products')
@@ -11,3 +12,6 @@ API.add_resource(admin_and_store_attendant_endpoints.GetSpecificProduct, '/produ
 API.add_resource(store_attendant_endpoint.StoreAttendant, '/saleorder')
 API.add_resource(admin_endpoints.SalesRecords, '/saleorder')
 API.add_resource(store_attendant_endpoint.SpecificSaleRecord, '/saleorder/<int:sale_id>')
+
+API_AUTH.add_resource(authorization.SignUp, '/signup')
+API_AUTH.add_resource(authorization.Login, '/login')
