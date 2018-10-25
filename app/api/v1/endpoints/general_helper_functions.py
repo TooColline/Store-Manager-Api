d77cbb451d4f072d7e77abeab0047302785f33a6
Too Collins <collinstoo.ct@gmail.com>
2018-10-25 12:52:11 +0300

    [Chore #161224891] Refactor helper functions

--- app/api/v1/endpoints/general_helper_functions.py
+++ app/api/v1/endpoints/general_helper_functions.py
@@ -42,52 +42,11 @@ def add_new_product(name, price, category):
         miss_parameter_required()
     return response
 
-def get_specific_product(product_id):
-    """General method to get a specific product from the store"""
-
-    specific_product = None
-
-    for product in ProductsModel.Products:
-        if product['product_id'] == product_id:
-            specific_product = product
-            break
-
-    if not specific_product:
-        abort_if_not_found(product_id)
-
-    return specific_product
-
 def abort_if_not_found(product_id):
     """General method to abort get request if the specific product is not in store"""
 
     abort(make_response(jsonify(
         message="Product with id number {} not found".format(product_id)), 404))
-
-def add_sale_record(name, price, quantity, totalamt):
-
-    """Creates a new sale record"""
-
-    if name and price and quantity and totalamt:
-        # If all the required data are provided
-        sale_id = len(SalesModel.salerec) + 1
-        SaleOrder = {
-            'sale_id': sale_id,
-            'name': name,
-            'price': price,
-            'quantity': quantity,
-            'totalamt': totalamt,
-            'date_added': datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        }
-
-        SalesModel.salerec.append(SaleOrder)
-        response = jsonify({
-            "message": "Sale record has been created successfully",
-            "SaleOrder": SalesModel.salerec[-1]})
-        response.status_code = 201
-    else:
-        # if any of the required parameters is missing or none
-        miss_parameter_required()
-    return response
 
 def get_specific_sale_record(sale_id):
     """Get specific record given a sale id"""
